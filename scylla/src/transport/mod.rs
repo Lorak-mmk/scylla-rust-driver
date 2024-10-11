1d83dbd64c0dd8dda49da838dbd1d1e7a228dec0
Karol Baryła <karol.baryla@scylladb.com>
2024-10-11 21:05:00 +0200

    atuhenticate_test: Move to integration

--- scylla/src/transport/mod.rs
+++ scylla/src/transport/mod.rs
@@ -25,8 +25,6 @@ pub use execution_profile::ExecutionProfile;
 pub use scylla_cql::frame::request::query::{PagingState, PagingStateResponse};
 
 #[cfg(test)]
-mod authenticate_test;
-#[cfg(test)]
 mod session_test;
 #[cfg(test)]
 mod silent_prepare_batch_test;
